0587b251a6f5938f7f45f6083f68bcfd2f5f188a
Kay van Bree <kayvanbree@gmail.com>
2020-02-19 12:13:38 +0100

    Move everything to plugins folder

---
+++ plugins/strapi-editorjs/controllers/StrapiEditorjs.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * StrapiEditorjs.js controller
+ *
+ * @description: A set of functions called "actions" of the `strapi-editorjs` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  }
+};
